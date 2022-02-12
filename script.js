b15c25d31eaa7c07783f6121fb63e2cce7571fab
Cybi1 <derenok_ilya@mail.ru>
2022-02-13 01:13:30 +0300

    Add initial implementation

---
+++ script.js
@@ -0,0 +1,32 @@
+const rock = "rock";
+const paper = "paper";
+const scissors = "scissors";
+const compVal = ['rock','paper','scissors'];
+let humanVal;
+
+function computerPlay() {   
+    let rand = Math.floor(Math.random()*compVal.length);
+    return compVal[rand];  
+}
+
+function humanPlay() {
+    let humanVal = prompt("Enter value:");
+    return humanVal;  
+}
+
+function roundPlay(compVal, humanVal) {
+    if (compVal == humanVal) {
+        return("draw");
+    } else if ((compVal === "rock" && humanVal === "paper")||
+               (compVal === "paper" && humanVal === "scissors")||
+               (compVal === "scissors" && humanVal === "rock")) {
+        return("win");
+    } else {
+        return("lose"); 
+    }
+}
+
+    
+let result = roundPlay(compVal(),humanVal());
+console.log(result);
+
