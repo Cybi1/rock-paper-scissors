27887d96923edab17ddfbe1b3d27928bf2369c7f
Cybi1 <derenok_ilya@mail.ru>
2022-02-14 01:44:46 +0300

    Fix more review issues

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
 const ROCK = "rock";
 const PAPER = "paper";
 const SCISSORS = "scissors";
-const ALL_SHAPES = [ROCK,PAPER,SCISSORS];
+const ALL_SHAPES = [ROCK, PAPER, SCISSORS];
 
 function computerPlay() {   
     const randomValue = Math.floor(Math.random() * ALL_SHAPES.length);
-    return ALL_SHAPES[randomValue];  
+    return ALL_SHAPES[randomValue]; 
 }
 
 function humanPlay() {
-    let humanVal = prompt("Enter value:");
-    return humanVal;  
+    return prompt("Enter value:");
 }
 
-function roundPlay(ALL_SHAPES,humanVal) {
-    if (ALL_SHAPES == humanVal) {
+function roundPlay(computerValue, humanValue) {
+    if (computerValue == humanValue) {
         return("draw");
-    } else if ((ALL_SHAPES === ROCK && humanVal === PAPER)||
-               (ALL_SHAPES === PAPER && humanVal === SCISSORS)||
-               (ALL_SHAPES === SCISSORS && humanVal === ROCK)) {
+    } else if ((computerValue === ROCK && humanValue === PAPER)||
+               (computerValue === PAPER && humanValue === SCISSORS)||
+               (computerValue === SCISSORS && humanValue === ROCK)) {
         return("win");
     } else {
         return("lose"); 
     }
 }
     
-let result = roundPlay(computerPlay(),humanPlay());
+let result = roundPlay(computerPlay(), humanPlay());
 console.log(result);
-
