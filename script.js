9997788a1bd5d929b97b279de81405dd04393657
Cybi1 <derenok_ilya@mail.ru>
2022-02-13 19:37:48 +0300

    changed variable name

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 const rock = "rock";
 const paper = "paper";
 const scissors = "scissors";
-const compVal = ['rock','paper','scissors'];
+const allShapes = ['rock','paper','scissors'];
 let humanVal;
 
 function computerPlay() {   
-    let rand = Math.floor(Math.random()*compVal.length);
-    return compVal[rand];  
+    let rand = Math.floor(Math.random()*allShapes.length);
+    return allShapes[rand];  
 }
 
 function humanPlay() {
     return prompt("Enter value:");  
 }
 
-function roundPlay(compVal, humanVal) {
-    if (compVal == humanVal) {
+function roundPlay(allShapes, humanVal) {
+    if (allShapes == humanVal) {
         return("draw");
-    } else if ((compVal === rock && humanVal === paper)||
-               (compVal === paper && humanVal === scissors)||
-               (compVal === scissors && humanVal === rock)) {
+    } else if ((allShapes === rock && humanVal === paper)||
+               (allShapes === paper && humanVal === scissors)||
+               (allShapes === scissors && humanVal === rock)) {
         return("win");
     } else {
         return("lose"); 
     }
 }
     
-let result = roundPlay(compVal(),humanVal());
+let result = roundPlay(allShapes(),humanVal());
 console.log(result);
 
